fd4e98467b54211cafa1ebb5dde9b0e56edd9151
Jon Thornburg <jon_thornburg@clickherelabs.com>
2016-10-17 14:28:50 -0500

    pulls data, loads tableview with correct # but all the same trail

--- TrailLocator/MakeTrailArray.swift
+++ TrailLocator/MakeTrailArray.swift
@@ -13,18 +13,19 @@ struct MakeTrailArray {
     static func makeTrails(term: String, completion: @escaping (_ trails: [TrailDict]) -> Void) {
         let trail = TrailDict()
         var trails = [TrailDict]()
-        let encodedTerm = term.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
-        let trailSearchString = "https://outdoor-data-api.herokuapp.com/api.json?api_key=d6d33ee90666c461d901c731cc104b79&q[city_cont]=\(encodedTerm)"
-        APIService.getDictionary(urlString: trailSearchString) { (dictionary) in
-            if let dict = dictionary {
-                if let places = dict["places"] as? [[String : AnyObject]]{
-                    for place in places {
-                        let newTrail = trail.fromDictionary(place)
-                        trails.append(newTrail)
+        if let encodedTerm = term.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) {
+            let trailSearchString = "https://outdoor-data-api.herokuapp.com/api.json?api_key=d6d33ee90666c461d901c731cc104b79&q[city_cont]=\(encodedTerm)"
+            APIService.getDictionary(urlString: trailSearchString) { (dictionary) in
+                if let dict = dictionary {
+                    if let places = dict["places"] as? [[String : AnyObject]]{
+                        for place in places {
+                            let newTrail = trail.fromDictionary(place)
+                            trails.append(newTrail)
+                        }
+                        DispatchQueue.main.async(execute: {
+                            completion(trails)
+                        })
                     }
-                    DispatchQueue.main.async(execute: { 
-                        completion(trails)
-                    })
                 }
             }
         }
